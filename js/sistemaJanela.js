fc67bc980df4a339ae58303a56dd5fb8959fb54e
Guest 01 <guest01@alunos.sc.senac.br>
2022-08-25 22:06:04 -0300

    third commit

---
+++ js/sistemaJanela.js
@@ -0,0 +1,17 @@
+function start() {
+    const menu = "#menu-ativo";
+    if ($(menu).is(":visible")) {
+        esconderElemento($(menu));
+    } else {
+        mostrarElemento($(menu));
+    }
+
+}
+
+function esconderElemento(ele) {
+    $(ele).hide()
+}
+
+function mostrarElemento(ele) {
+    $(ele).show()
+}
